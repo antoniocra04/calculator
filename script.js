63f8b4a49babee2e8e9e73c8fc5acc87ed35b9f6
Anton <anton@Antons-Laptop.local>
2025-07-23 17:32:42 +0700

    добавляем логику кнопок

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 const display = document.querySelector('.display_input');
-display.value = '0';
+
+document.querySelectorAll('.button').forEach(button => {
+  const id = button.id;
+
+  button.addEventListener('click', () => {
+    switch (id) {
+      case 'clear':
+        display.value = '';
+        break;
+      case 'backspace':
+        display.value = display.value.slice(0, -1);
+        break;
+      default:
+        if(button.textContent === '0' && (display.value === '0' || display.value === '')) {
+            return;
+        }
+        display.value += button.textContent;
+    }
+  });
+});
+
+document.querySelector('.equals_button').addEventListener('click', () => {
+  try {
+    display.value = eval(clean);
+  } catch {
+    display.value = 'Error';
+  }
+});
